c8d7c35b883fe20d79aa6be3fc2fb643dd24f49d
hy8915 <89782375+hy8915@users.noreply.github.com>
2021-08-30 21:22:38 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+<<<<<<< HEAD
+const myHeading = document.querySelector('h1')
+
+=======
+const myHeading = document.querySelector('h1')
+
+>>>>>>> e9ae099f407806a2d8da2a94b91393c64d11299f
+myHeading.textContent = '안녕하세요! 자바스크립트'
